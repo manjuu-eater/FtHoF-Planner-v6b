4ba22daed049893693df6bcf5ec0714803c6137c
Ika4 <sandtsorm@yahoo.co.jp>
2025-02-22 19:55:43 +0900

    rename Cookie -> FthofResult

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { Math_seedrandom, choose, M_spells } from "./game_related_data.js";
 
 // type definition
 /**
- * @typedef {object} Cookie
+ * @typedef {object} FthofResult
  * @property {string} type
  * @property {boolean} wrath
  * @property {string} description
@@ -427,7 +427,7 @@ app.controller('myCtrl', function ($scope) {
 	 * @param {number} spellsCastTotal total spell cast count before this cast
 	 * @param {boolean} isOneChange true if one change
 	 * @param {("GC" | "RC")=} forceCookie "GC": force GC, "RC": force RC, default: roll with Math.random()
-	 * @returns {Cookie} FtHoF cast result
+	 * @returns {FthofResult} FtHoF cast result
 	 */
 	const castFtHoF = (spellsCastTotal, isOneChange, forceCookie) => {
 		// set seed (L312)
